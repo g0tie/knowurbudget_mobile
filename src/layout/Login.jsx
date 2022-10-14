0fb0eeb72367a14a18628ecf9a06f8e1ab9a767a
g0tie <gaultierbegue@outlook.com>
2022-10-14 09:27:44 +0400

    remove useless img on login

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -56,7 +56,6 @@ const Login = ({}) => {
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div className="max-w-md w-full space-y-8">
     <div>
-      <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow" />
       <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Connexion</h2>
     </div>
     <button onClick={() => navigate(-1)}>
--- src/layout/Register.jsx
+++ src/layout/Register.jsx
@@ -47,7 +47,6 @@ const Register = ({}) => {
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div className="max-w-md w-full space-y-8">
     <div>
-      <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow" />
       <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Inscription</h2>
       <p className="mt-2 text-center text-sm text-gray-600">
       </p>
